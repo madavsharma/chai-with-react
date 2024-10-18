2810e2c6f41e91ff6267b0d5839e06186ad52a65
Manik Sharma <114878881+madavsharma@users.noreply.github.com>
2024-10-18 22:29:57 +0530

    Created UI components for the result page

--- quizApp/src/components/Result.jsx
+++ quizApp/src/components/Result.jsx
@@ -1,25 +1,25 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import '../styles.css';
 
-const Result = () => {
+const Score = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const score = new URLSearchParams(location.search).get('score');
-  const totalQuestions = 2; // Update this value to reflect the total number of questions
+  const totalQuestions = 10; 
   const message = score >= totalQuestions / 2 ? "Great job!" : "Thanks for participating!";
 
   return (
     <div className="result-container">
       <h1>Your Result</h1>
-      <div className="score">{score} / {totalQuestions}</div> {/* Display the score */}
-      <p className="result-message">{message}</p> {/* Display message based on score */}
-      <div className="button-container"> {/* New container for centering buttons */}
+      <div className="score">{score} / {totalQuestions}</div>
+      <p className="result-message">{message}</p>
+      <div className="button-container">
         <button onClick={() => navigate('/')} className="play-again-button">
-          <i className="fas fa-redo"></i> Play Again {/* Redo icon */}
+          <i className="fas fa-redo"></i> Play Again 
         </button>
       </div>
     </div>
   );
 };
 
-export default Result;
+export default Score;
